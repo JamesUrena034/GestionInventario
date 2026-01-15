b5c95dbeb71c4ea1db2c34935ab1731e08372397
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-01-15 18:07:56 -0400

    feat ✨: nuevo modelo para el producto

---
+++ Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionEntradasInventario.Models;
+
+public class Producto
+{
+    [Key]
+    public int ProductoId { get; set; }
+
+    [Required(ErrorMessage = "Debe tener una descripción")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "El costo debe ser mayor a 0")]
+    public double Costo { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    public double Precio { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "La existencia no puede ser negativa")]
+    public double Existencia { get; set; }
+}
