c4c378149fad02d8f257f5f1b16697e37216a291
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-01-15 18:05:47 -0400

    feat ✨: Realiza el modelo Entradas

---
+++ Models/Entradas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionEntradasInventario.Models;
+
+public class Entradas
+{
+    [Key]
+    public int EntradaId { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "El concepto es obligatorio")]
+    public string Concepto { get; set; } = string.Empty;
+
+    [Range(0, double.MaxValue, ErrorMessage = "El total no puede ser negativo")]
+    public double Total { get; set; }
+
+    [InverseProperty("Entrada")]
+    public virtual ICollection<EntradasDetalle> EntradasDetalle { get; set; } = new List<EntradasDetalle>();
+}
